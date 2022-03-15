f8a1cc0906929a03e23236da375893796365c31e
ApostleB <jbu1391@gmail.com>
2022-03-16 01:09:17 +0900

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
       <App />
+    </ThemeProvider>
   </React.StrictMode>,
+
   document.getElementById("root")
 );
